8d8b813ad501f137c8ac5df812593449a4fe7b14
Syed Shayan <shayansyed712@gmail.com>
2023-12-14 22:27:11 +0500

    change

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { Text, View } from 'react-native';
 import OnBoard from './src/onboard/OnBoard';
 import Home from './src/home/Home';
+import Footer from './src/components/footer/Footer';
 
 const App = () => {
   return (
     <View>
       {/* <OnBoard /> */}
-      <Home />
+      {/* <Home /> */}
+      <Footer />
     </View>
   );
 };
