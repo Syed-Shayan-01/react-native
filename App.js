a6eeffa5e3e6fe43e3fd02c6d05e6255b9c00519
Syed Shayan <shayansyed712@gmail.com>
2023-12-16 21:50:23 +0500

    new chnges in package.json

--- App.js
+++ App.js
@@ -1,93 +1,13 @@
-// import { Text, View } from 'react-native';
-import OnBoard from './src/onboard/OnBoard';
-// import Home from './src/home/Home';
-import Footer from './src/components/footer/Footer';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native';
-import { Ionicons } from 'react-native-vector-icons/Ionicons';
-const Tab = createBottomTabNavigator();
-
-const Home = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Home Screen</Text>
-  </View>
-);
-
-const About = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>About Screen</Text>
-  </View>
-);
-
-const Profile = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Profile Screen</Text>
-  </View>
-);
-
-const Cart = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Cart Screen</Text>
-  </View>
-);
+import { View } from 'react-native'
+import React from 'react'
+import AppNavigator from './src/appNavigator/AppNavigator'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: 'blue',
-          inactiveTintColor: 'gray',
-          style: {
-            backgroundColor: 'white',
-            borderTopWidth: 1,
-            borderTopColor: 'lightgray',
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="About"
-          component={About}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="information-circle" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={Cart}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="cart" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View>
+      <AppNavigator />
+    </View>
+  )
+}
 
-
-
-export default App;
+export default App
---
+++ src/appNavigator/AppNavigator.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+const AppNavigator = () => {
+  return (
+    <View>
+      <Text>
+        
+      </Text>
+    </View>
+  )
+}
+
+export default AppNavigator
